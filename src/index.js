d7fa5fef40a5cdfbd7967e817c75b80b04bed3b5
James McCann <jamesredux@gmail.com>
2020-08-17 16:28:47 +0100

    set up webpack and dev server

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import './reset.css';
+import './style.css';
+
+function component() {
+    const element = document.createElement('div');
+  
+    // Lodash, currently included via a script, is required for this line to work
+    element.innerHTML =  `<h1>To Do App</h1>`;
+  
+    return element;
+  }
+  
+  document.body.appendChild(component());
